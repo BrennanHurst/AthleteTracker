4e552aeda1d7bd6c14af7b441383f5d77813f451
brennanhurst <brennanhurst@10.3.160.40>
2017-03-09 09:35:45 -0500

    v

--- AthleteTracker/src/com/classproject/GUI/components/GUIComponent.java
+++ AthleteTracker/src/com/classproject/GUI/components/GUIComponent.java
@@ -1,5 +1,7 @@
 package com.classproject.GUI.components;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 import com.classproject.GUI.GUIConstants;
@@ -19,7 +21,43 @@ public abstract class GUIComponent extends JPanel{
 	 */
 	public void configureLayout(){
 		this.setBounds(0, 0, GUIConstants.SCREENWIDTH, GUIConstants.SCREENHEIGHT);
+		this.setBackground(Color.WHITE);
+		this.setVisible(true);
+	
 	};
+	
+	
+	
+	
+	
+}
+		this.setBackground(Color.WHITE);
+		this.setVisible(true);
+	
+	};
+	
+	
+	
+	
+	
+}
+EENHEIGHT);
+		this.setBackground(Color.WHITE);
+		this.setVisible(true);
+	
+	};
+	
+	
+	
+	
+	
+}
+		this.setBackground(Color.WHITE);
+		this.setVisible(true);
+	
+	};
+	
+	
 	
 	
 	
